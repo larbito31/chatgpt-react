6e8d3b161929145ecedf2a37ee6df505f6013e35
larbito31 <boukhobza.larbi31@gmail.com>
2023-05-30 17:12:47 +0200

    Update styles and layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
 
 const App =  () => {
   return (
-    <div className="App">
+    <div className="app">
+
+      <section className="side-bar"> 
+      <button>+ New chat</button>
+      <ul className="history">
+        <li> TESt TEST</li>
+      </ul>
+      <nav>
+        <p> made by Larbi</p>
+      </nav>
+      
+      </section>
+      <section className="main"> 
+        <h1>PersonalGPT</h1>
+        <ul className="feed">
+
+        </ul>
+        <div class="bottom-section">
+          <div class="input-container"> 
+            {/* <input/> */}
+            <input type="text" placeholder="Send a message..."></input>
+            <div id="submit">➢</div>
+          </div>
+          <p className="info">
+          ChatGPT may produce inaccurate information about people, places, or facts. ChatGPT May 24 Version
+          </p>
+        </div>
+      </section>
     </div>
   );
 }
